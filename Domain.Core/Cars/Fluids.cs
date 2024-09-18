84fda8b7d419d13fe502ae3eb9eaeebbddd1d8e6
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-09-18 12:18:42 +0500

    FEAT: add fluids

---
+++ Domain.Core/Cars/Fluids.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Core.Cars
+{
+    public class Fluids : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public double PricePerUnit { get; set; }
+    }
+}
