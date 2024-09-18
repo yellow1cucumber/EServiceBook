0f4892761ac36da55e8281fe89df7a5027e62a70
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-09-18 12:16:18 +0500

    FEAT: add car units

---
+++ Domain.Core/Cars/CarUnits/CarUnit.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Core.Car.CarParts;
+
+namespace Domain.Core.Cars.CarUnits
+{
+    public class CarUnit : Entity
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<CarPart> Parts { get; set; } = [];
+    }
+}
