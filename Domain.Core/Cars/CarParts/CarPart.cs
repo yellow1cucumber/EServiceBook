008841fa2d9a70a036d4715e1d39693103c88764
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-09-18 12:11:54 +0500

    FEAT: add car part, car part changes, car part maintenances

---
+++ Domain.Core/Cars/CarParts/CarPart.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Core.Cars.CarParts;
+
+namespace Domain.Core.Car.CarParts
+{
+    public class CarPart : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public IEnumerable<Maintenance> Maintenances { get; set; } = [];
+        public IEnumerable<Change> Changes { get; set; } = [];
+    }
+}
---
+++ Domain.Core/Cars/CarParts/Maintenance.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Core.Cars.CarParts
+{
+    public class Maintenance : Entity
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public double Mileage { get; set; }
+        public double? MotorHours { get; set; } = null;
+
+        public double Price { get; set; }
+    }
+}
