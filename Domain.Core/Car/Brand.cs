f720e1a332d0e72ba44a953a061c793f6466f9b6
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-09-18 11:36:32 +0500

    FEAT: add car brand

---
+++ Domain.Core/Car/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Core.Car
+{
+    public class Brand : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Logo { get; set; } = string.Empty;
+    }
+}
