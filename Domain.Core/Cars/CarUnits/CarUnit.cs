bf3d00ffceab6ba16054ed059657d78793a932f2
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-09-18 12:20:27 +0500

    CHORE: add string.Empty to close warning

--- Domain.Core/Cars/CarUnits/CarUnit.cs
+++ Domain.Core/Cars/CarUnits/CarUnit.cs
@@ -4,7 +4,7 @@ namespace Domain.Core.Cars.CarUnits
 {
     public class CarUnit : Entity
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         public IEnumerable<CarPart> Parts { get; set; } = [];
         public IEnumerable<Fluids> Fluids { get; set; } = [];
