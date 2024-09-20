ed8d6fa8c0784fa8b6cdf3c83fb88f0c399d4d0c
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-09-20 13:24:53 +0500

    FEAT: add graphql service extension

---
+++ Infrastructure.GraphQL/GQLServiceExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.GraphQL
+{
+    public static class GQLServiceExtension
+    {
+        public static IServiceCollection AddGQLService(this IServiceCollection services)
+        {
+            services.AddGraphQLServer()
+                    .AddProjections()
+                    .AddFiltering()
+                    .AddSorting();
+
+
+            return services;
+        }
+    }
+}
